d0cf7a5dfa683ba3fee82115138bdf63f7d04992
SeniorDmitriy <dimaponomarenko443@gmail.com>
2022-10-27 17:41:55 +0300

    added postgres and typeorm. Create datavase in docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './user/user.entity';
 
 const enviroment = process.env.NODE_ENV || 'development';
 @Module({
   imports: [
     UserModule,
-    AuthModule,
+    // AuthModule,
     ConfigModule.forRoot({
       envFilePath: `.env.${enviroment}`,
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGODB_WRITE_CONNECTION_STRING),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      username: 'admin',
+      port: 5432,
+      password: 'root',
+      database: 'postgres',
+      entities: [User],
+      synchronize: true,
+    }),
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,3 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-
 @Injectable()
-export class AuthService {
-  constructor(private readonly configService: ConfigService) {
-    const mongoConnectionStr = this.configService.get<string>(
-      'MONGODB_WRITE_CONNECTION_STRING',
-    );
-
-    console.log('!!MONGODB_WRITE_CONNECTION_STRING', mongoConnectionStr);
-  }
-}
+export class AuthService {}
